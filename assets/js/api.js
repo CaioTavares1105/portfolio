29b13c02d3ba1658a7d773bcd5ea3728cb4afd90
Caio <caioccca1105@gmail.com>
2024-02-09 18:08:50 +0000

    atualização API

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData () {
-    const url = "https://raw.githubusercontent.com/CaioTavares1105/js-developer-portfolio/main/data/profile.json";
+    const url = "https://raw.githubusercontent.com/CaioTavares1105/portfolio/main/data/profile.json";
     const fetching = await fetch(url)
     return await fetching.json()
 }
